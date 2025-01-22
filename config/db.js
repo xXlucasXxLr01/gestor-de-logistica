610281d72a27e1a0d476523c1c1400ef0b013fab
lucas alvares <lucas_alvares@hotmail.com>
2025-01-21 23:02:17 -0300

    conexion a atlas

--- config/db.js
+++ config/db.js
@@ -1,12 +1,13 @@
-const mongose = require('mongoose');
-const dbconnect =  async () => {
-    try{
-        await mongose.connect('mongodb://localhost:27017/dbGOLogistica')
-            console.log('conectado a la base de datos');
+require('dotenv').config();
+const mongoose = require('mongoose');
 
-        } catch (error) {
-            console.error('Error de coneccion a la base de datos', error);
-            ;
-        }
+const dbconnect = async () => {
+    try {
+      await mongoose.connect(process.env.MONGO_URI);
+      console.log("✅ Conexión exitosa a MongoDB Atlas");
+    } catch (error) {
+      console.error("❌ Error al conectar a MongoDB Atlas:", error.message);
     }
-module.exports = dbconnect;
+  };
+
+  module.exports = dbconnect;
